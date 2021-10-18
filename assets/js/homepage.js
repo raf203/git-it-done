69ed48df75703751ca42a54e59228739fc4d764a
Rafael Cardoso <rrafa203@gmail.com>
2021-10-18 14:38:23 -0700

    Add form and javascript to get info of the prompt

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,5 +1,41 @@
-var getUserRepos = function () {
-    fetch("https://api.github.com/users/octocat/repos");
+
+
+var response = fetch("https://api.github.com/users/octocat/repos").then(function(response){
+    response.json().then(function(data){
+        console.log(data)
+;    });
+});
+
+var userFormEl = document.querySelector("#user-form");
+var nameInputEl = document.querySelector("#username");
+
+var formSubmitHandler = function(event) {
+    event.preventDefault ();
+
+    // get value from input element
+    var username = nameInputEl.value.trim();
+
+    if (username) {
+        getUserRepos(username);
+        nameInputEl.value = "";
+    } else {
+        alert("Please enter a GitHub username");
+    }
+
+    console.log(event);
 };
 
-getUserRepos();
+
+var getUserRepos = function (user) {
+    //format the git hub api url
+    var apiUrl = "https://api.github.com/users/" + user + "/repos";
+
+    // make a request to the url
+    fetch(apiUrl).then(function(response) {
+        response.json().then(function(data) {
+            console.log(data);
+        });
+    });
+};
+
+userFormEl.addEventListener("submit", formSubmitHandler);
